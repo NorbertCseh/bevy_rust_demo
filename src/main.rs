d60d9bb25a083b430fdf6cf6125613e14d47d63f
NorbertCseh <cseh.norbert94@gmail.com>
2022-02-14 10:07:03 +0100

    Progress on the tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new().add_startup_system(add_people).add_system(hello_world).add_system(greet_people).run();
-}
-
-fn hello_world() {
-    println!("Hello world");
-}
-
 #[derive(Component)]
 struct Name(String);
 
 #[derive(Component)]
 struct Person;
 
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
-    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
-    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+pub struct HelloPlugin;
+struct GreetTimer(Timer);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
+    }
+}
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
     }
 }
